506db2701fff6b646521db87310df34c15b16a62
Becca Cardwell <42225076+beccatcardwell@users.noreply.github.com>
2024-01-04 20:39:31 -0500

    Added CRUD methods fort users

--- routes/users.js
+++ routes/users.js
@@ -25,35 +25,76 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(users)
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email
+  let user = users.filter(item => item.email === email)
+  res.send(user)
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const query = req.query
+  users.push({
+    firstName: query.firstName,
+    lastName: query.lastName,
+    email: query.email,
+    DOB: query.DOB
+  })
+  res.send('The user ' + query.firstName + ' ' + query.lastName + ' has been added!')//This line is to be replaced with actual return value
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email
+  let filtered_users = users.filter(item => item.email.toLowerCase() === email.toLowerCase())
+
+  if (filtered_users.length > 0) {
+    const user = filtered_users[0]
+    const query = req.query
+    const DOB = query.DOB
+    const firstName = query.firstName
+    const lastName = query.lastName
+  
+    if (DOB) {
+      user.DOB = DOB
+    }
+
+    if (firstName) {
+      user.firstName = firstName
+    }
+
+    if (lastName) {
+      user.lastName = lastName
+    }
+
+    users = users.filter((user) => user.email.toLowerCase() != email.toLowerCase())
+    users.push(user)
+    res.send(`User with the email ${email} updated.`)
+
+  } else {
+    res.send(`Could not find user with email matching ${email}.`)
+  }
+
 });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email.toLowerCase()
+  let filtered_users = users.filter(item => item.email.toLowerCase() === email)
+
+  if (filtered_users.length > 0) {
+    users = users.filter(item => item.email.toLowerCase() !== email)
+    res.send(`User with the email ${email} deleted.`)
+  } else {
+    res.send(`Could not find user with email matching ${email}.`)
+  }
 });
 
 module.exports=router;
